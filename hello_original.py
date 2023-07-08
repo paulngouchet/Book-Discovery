145b3ee88ffe5eae8856aa62dedb8014486c3600
paulngouchet <ngouchet@bu.edu>
2023-07-08 01:48:41 +0100

    Cleaning

--- hello_original.py
+++ hello_original.py
@@ -1,29 +1,15 @@
 from flask import Flask, render_template, request
-
 import scrapy
-
 import time
-
 from scrapy.crawler import CrawlerProcess
-
 from subprocess import call
-
-
-
 import pickle
-
 import json
-
 import os
-
 import os.path
 from os import path
-
 from os import getcwd
-
-
 import requests
-
 from mongoengine import *
 connect('book_search_db', host='localhost', port=27017)
 
@@ -31,20 +17,12 @@ connect('book_search_db', host='localhost', port=27017)
 # For optimization and speed, My code will certainly need lot of caching because i am going over the same arrays data many times
 
 
-cx= '005331999340111559203:nb9u1-ynjkw'
-
-googleapikey="AIzaSyCbkWvy9ETOPHh5_lvsRVrdEQNiZt7mlHQ"
-
-
+cx= ''
+googleapikey=""
 dict_url_book = {}
-
 frontend_data = [] # Use Jinja, array of all the book information, [ index 0 - Book_name, index 1 - product_id,  index 3 - amazon_url, index 4 - Google book url ]
-
 other_frontend_data = [] # Just index 0 - the product_id, index 1 - amazon_url
-
-
-
-
+app = Flask(__name__)
 
 class Urls(Document):
     url = StringField()
@@ -69,7 +47,6 @@ def search_book(value):
     r = requests.get(url="https://www.googleapis.com/books/v1/volumes", params=parms)
     print r.url
     rj = r.json()
-
     print(rj["items"][0]["volumeInfo"]["title"])
 
     return rj["items"][0]
@@ -79,13 +56,10 @@ def search_web(value):
     parms = {"q":value,  'key':googleapikey, 'cx':cx, 'num':10}  # 10 maximum results
     # For restricted queries, use this version of the requests
     #r = requests.get(url="https://www.googleapis.com/customsearch/v1/siterestrict", params=parms)
-
     # For the non restricted version - use - 10K query limit a day - 100 free - $ 5 for every 1000 queries
     r = requests.get(url="https://www.googleapis.com/customsearch/v1", params=parms)
     print r.url
     rj = r.json()
-
-
     list_website_url = []
 
     for web in rj["items"]:
@@ -94,21 +68,15 @@ def search_web(value):
     return list_website_url
 
 
-
-
 def amazon_url(id):
-
     format = 'http://amzn.com/'
     product_id = id
     amazon_id = 'paulngouche0c-20'
-
     result = format + id + '/?tag=' + amazon_id
-
     return result
 
 
 def process_dict_url(urls):
-
     data = []
     db_book_name = {} # For mongo db, string : string
     db_product_id = {} # For mongo db, string : array string
@@ -129,10 +97,8 @@ def process_dict_url(urls):
                 data.append(amazon)
                 other_frontend_data.append(data)
                 print("other_frontend_data", other_frontend_data)
-
                 product1 = Product_ids(id_number=book[0], info=str([amazon])) # save in db
                 product1.save()
-
                 print("only url", amazon)
             else:
 
@@ -143,22 +109,14 @@ def process_dict_url(urls):
 
                 product1 = Product_ids(id_number=book[0], info=str([amazon, book[1]])) # Save in the database
                 product1.save()
-
                 result = search_book(book[1]) # Search Each book
                 data.append(result)
                 frontend_data.append(data)
                 print("frontend_data", frontend_data)
-
                 book1 = Books(name=book[1], info=str(result))
                 book1.save()
-
                 print("url and book", amazon, book[1])
 
-
-
-
-
-app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
@@ -169,13 +127,10 @@ def hello_world():
 def query_books():
     if request.method == 'POST':
         query = request.form['search']
-
         search_query = query
         url_searches = search_web(search_query)
-
         search = Searches(query=search_query, urls=str(url_searches))
         search.save()
-
         print("getting here")
 
         with open('outfile', 'wb') as fp:
@@ -192,15 +147,9 @@ def query_books():
         with open(path_file) as json_file:
             data = json.load(json_file)
             dict_url_book = data[-1]
-
-
         print("getting here")
         #print(dict_url_book)
-
-
-
         process_dict_url(dict_url_book)
-
         print(len(frontend_data))
         #return "success"
         return str(frontend_data) # To be fixed
